f8d3132ee970b4818343f044063a36c92edccb27
Hyeonki <106725881+noranfox@users.noreply.github.com>
2023-04-26 20:43:19 +0900

    InputAction 기본세팅완료

---
+++ Assets/Script/PlayercController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayercController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector2 moveVec;
+    
+    
+}
